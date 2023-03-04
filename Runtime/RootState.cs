613e8d62b69e1ec15e11bb507099414d08fa4a2c
Giseudo <giseudo@gmail.com>
2023-03-04 19:58:37 -0300

    Implemented initial state machine context

--- Runtime/RootState.cs
+++ Runtime/RootState.cs
@@ -4,5 +4,13 @@ namespace HFSM
 {
     [Serializable]
     public class RootState : State
-    { }
+    {
+        private StateMachineContext _context;
+        public T Context<T>() where T : StateMachineContext => _context as T;
+
+        public void SetContext(StateMachineContext context)
+        {
+            _context = context;
+        }
+    }
 }
